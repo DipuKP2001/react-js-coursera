488cf3d3d931fa5f940eb986ad33a6d55750c799
DipuKP2001 <dipukp602@gmail.com>
2020-04-19 20:23:28 +0530

    basic implementation

---
+++ sample/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/" target="_blank">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
