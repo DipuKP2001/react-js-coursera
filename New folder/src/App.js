a545ff71256d48c67748505daed814403775d251
DipuKP2001 <dipukp602@gmail.com>
2020-04-20 12:45:34 +0530

    second commit

---
+++ New folder/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
+import './App.css';
+import { DISHES } from './shared/dishes.js';
+
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			dishes: DISHES
+		};
+	}
+	render() {
+		return (
+		<div className="App">
+		    <Navbar dark color="primary">
+		        <div className="container">
+		        	<NavbarBrand href="/" target="_blank">Ristorante Con Fusion</NavbarBrand>
+		        </div>
+		    </Navbar>
+		    <Menu dishes={this.state.dishes} />
+		</div>
+		);
+	}
+}
+
+export default App;
